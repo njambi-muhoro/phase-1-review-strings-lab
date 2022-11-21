6dad12eeb484c88a3a1d8c020017d216ece2489c
+ <+>
2022-11-21 22:54:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = "Welcome to Flatbook";
+let welcomeMessage ="Welcome to Flatbook, !";
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+let shortGreeting =`${currentUser[0].slice(0,1)}elcome, to Flatbook !`;
